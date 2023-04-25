75fbe21c7afb7dce6be0e75e3c6d3a0e00a08396
Sean McQuay <srmcquay@gmail.com>
2023-04-25 16:40:21 +0000

    Added foundation of development container.

---
+++ scripts/postCreateCommand.sh
@@ -0,0 +1,12 @@
+sudo apt update
+
+sudo apt install curl -y
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+source ~/.bashrc
+
+nvm install 18
