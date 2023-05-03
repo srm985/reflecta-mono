8fdf994e0b34bbc69ea2dd9064cdb7f804e39d9b
Sean McQuay <srmcquay@gmail.com>
2023-05-03 12:00:22 +0000

    Testing updated CI/CD pipeline.

--- packages/reflecta-ui/src/App.tsx
+++ packages/reflecta-ui/src/App.tsx
@@ -2,7 +2,7 @@ import ButtonComponent from './components/remote/ButtonComponent';
 
 const App = () => (
     <div>
-        <p>hello NEW1011</p>
+        <p>hello NEW10112</p>
         <ButtonComponent label={'hey! foo'} />
     </div>
 );
