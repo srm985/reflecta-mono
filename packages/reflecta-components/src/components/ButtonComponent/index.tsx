00fb030c8930e43c38428385c268e9da7bb0e93e
Sean McQuay <srmcquay@gmail.com>
2023-05-03 12:29:37 +0000

    Testing updated CI/CD pipeline.

--- packages/reflecta-components/src/components/ButtonComponent/index.tsx
+++ packages/reflecta-components/src/components/ButtonComponent/index.tsx
@@ -18,7 +18,7 @@ const ButtonComponent: React.FC<IButtonComponent> = (props) => {
             className={displayName}
             type={'button'}
         >
-            {`FOO11111:${label}`}
+            {`prod:${label}`}
         </button>
     );
 };
