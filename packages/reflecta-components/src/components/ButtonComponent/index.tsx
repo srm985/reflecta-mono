71ae023c0f8bd1014de84351c5fce698fbaabf9c
Sean McQuay <srmcquay@gmail.com>
2023-05-03 10:08:46 +0000

    Testing updated CI/CD pipeline.

--- packages/reflecta-components/src/components/ButtonComponent/index.tsx
+++ packages/reflecta-components/src/components/ButtonComponent/index.tsx
@@ -18,7 +18,7 @@ const ButtonComponent: React.FC<IButtonComponent> = (props) => {
             className={displayName}
             type={'button'}
         >
-            {`FOO4:${label}`}
+            {`FOO5:${label}`}
         </button>
     );
 };
--- packages/reflecta-ui/src/App.tsx
+++ packages/reflecta-ui/src/App.tsx
@@ -2,7 +2,7 @@ import ButtonComponent from './components/remote/ButtonComponent';
 
 const App = () => (
     <div>
-        <p>hello NEW4</p>
+        <p>hello NEW5</p>
         <ButtonComponent label={'hey! foo'} />
     </div>
 );
