fbfbb8711a611af86deed60b32d0517aa2fa047a
Sean McQuay <srmcquay@gmail.com>
2024-01-22 10:38:49 +0000

    Bumped package versions and updated dev container.

--- scripts/postCreateCommand.sh
+++ scripts/postCreateCommand.sh
@@ -1,8 +1,10 @@
 sudo apt update
 
+# Install nvm
 sudo apt install curl -y
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 
+# Configure nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
@@ -20,11 +22,16 @@ echo "fi" >> ~/.bashrc
 source ~.bashrc
 source /tmp/.bashrc
 
-nvm install 18
+# Install the latest version of NodeJS LTS
+nvm install --lts
 
+# Update NPM
 npm install -g npm@latest
 
+# Install package dependencies
 npm ci
 
+# Allow execution
 chmod ug+x .husky/*
 chmod ug+x .git/hooks/*
+find ./scripts/ -type f -regex '.*\.\(py\|sh\|bash\)$' -exec chmod +x {} +
